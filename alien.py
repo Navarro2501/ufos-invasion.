b4d365d23811aca46dbaa57d0ae403308b3f2956
Navarro2501 <kevin.navarrogrz@gmail.com>
2019-12-04 15:26:02 -0600

    Added ship, bullets and alien animations and collition functionality

---
+++ alien.py
@@ -0,0 +1,36 @@
+import pygame
+
+from pygame.sprite import Sprite
+
+class Alien(Sprite):
+	#Controls the alien class
+	
+	def __init__(self, ai_game):
+		#Initialize the alien and sets its starting position
+		super().__init__()
+		self.screen = ai_game.screen
+		self.settings = ai_game.settings
+
+		#Loads the alien image
+		self.image = pygame.image.load('images/alien.bmp')
+		self.rect = self.image.get_rect()
+
+		#Start each new alien near the top left of the screen
+		self.rect.x = self.rect.width
+		self.rect.y = self.rect.height
+
+		#Store the alien's exact horizontal position
+		self.x = float(self.rect.x)
+
+
+	def update(self):
+		#Move the alien right or left
+		self.x += (self.settings.alien_speed * self.settings.fleet_direction)
+		self.rect.x = self.x
+
+
+	def check_edges(self):
+		#Return True if alien is at edge of the screen
+		screen_rect = self.screen.get_rect()
+		if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+			return True
